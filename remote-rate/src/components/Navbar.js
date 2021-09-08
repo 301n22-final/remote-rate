d04bd5d1f908446c860a6666505e4cb120886064
Zach-Winterton <schenley67@gmail.com>
2021-09-07 21:47:00 -0600

    moved about us on navbar

--- remote-rate/src/components/Navbar.js
+++ remote-rate/src/components/Navbar.js
@@ -38,6 +38,7 @@ function Navbar(props) {
             })}
             {props.isAuthenticated?
              <li>
+          
              <LogoutButton />
             </li>: <LoginButton />
             }
